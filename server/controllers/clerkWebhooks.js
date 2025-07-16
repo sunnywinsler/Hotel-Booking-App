65a60f2fdf1dd54d9c12f4641ca49ce5a879326c
Sunny Kumar <sunnywinsler1@gmail.com>
2025-07-16 18:32:21 +0530

    Updated

--- server/controllers/clerkWebhooks.js
+++ server/controllers/clerkWebhooks.js
@@ -17,26 +17,39 @@ router.post("/", async (req, res) => {
      await whook.verify(JSON.stringify(req.body), headers);
     const { data, type } = req.body;
 
+    
+
+switch (type) {
+  case "user.created": {
     const userData = {
       _id: data.id,
       email: data.email_addresses[0].email_address,
       username: data.first_name + " " + data.last_name,
       image: data.image_url,
     };
+    await User.create(userData);
+    break;
+  }
 
-    switch (type) {
-      case "user.created":
-        await User.create(userData);
-        break;
-      case "user.updated":
-        await User.findByIdAndUpdate(data.id, userData);
-        break;
-      case "user.deleted":
-        await User.findByIdAndDelete(data.id);
-        break;
-      default:
-        break;
-    }
+  case "user.updated": {
+    const userData = {
+      _id: data.id,
+      email: data.email_addresses[0].email_address,
+      username: data.first_name + " " + data.last_name,
+      image: data.image_url,
+    };
+    await User.findByIdAndUpdate(data.id, userData);
+    break;
+  }
+
+  case "user.deleted":
+    await User.findByIdAndDelete(data.id);
+    break;
+
+  default:
+    break;
+}
+
 
     res.json({ success: true, message: "Webhook Received Successfully" });
   } catch (error) {
